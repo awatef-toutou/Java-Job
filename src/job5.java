96c048e8586595f416ed8305b3ae694a8e306a85
awatef-toutou <awatefnf@hotmail.fr>
2025-10-28 10:36:28 +0100

    last evening

--- src/job5.java
+++ src/job5.java
@@ -3,9 +3,17 @@ public class job5 {
     static void main(String[] args) {
 
         for( int i=0; i<=1000; i++)
-        { if(i%1==0 && i%i=1)
+          {
+              for(int j = 2 ; j<i ; j++ )
+              {
+                  if(i % j == 0) {
+                      break;
+                  } else {
+                      System.out.println(i);
+                  }
 
-        }
+              }
+          }
 
 
 
