82c8dbb016dd352fde7094e793c016b75adcd2ce
awatef-toutou <awatefnf@hotmail.fr>
2025-10-29 15:54:59 +0100

    last evening

--- src/job9.java
+++ src/job9.java
@@ -3,8 +3,7 @@ public class job9 {
 
         int h = 5;
 
-        for(int i=1; i<=h; i++) {
-            for(int j=2*h; j>=i; j--){
+        for(int i=1; i<=h; i++) {for(int j=2*h; j>=i; j--){
                 System.out.print(" ");//creer le vide de haut
             }
             for(int j=1; j<=2*i-1; j++) { //on dessine les etoiles
