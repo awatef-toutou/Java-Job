2228310fe21b1b0dc23ab796cf6e6a60e7ce7356
awatef-toutou <awatefnf@hotmail.fr>
2025-11-10 15:32:56 +0100

    evening

--- src/BouclesTableaux/ex12.java
+++ src/BouclesTableaux/ex12.java
@@ -3,30 +3,40 @@ package BouclesTableaux;
 import java.util.Scanner;
 
 public class ex12 {
-    public static void main (String[] args)
-    {
+    public static void main(String[] args) {
         password();
     }
-     static void password()
-     {
-         String pwd = "Q941";
-         Scanner c= new Scanner(System.in);
-         System.out.println("------------- Guess the password -------------");
-         System.out.println("It must contain a maximum of 4 characters");
-         System.out.println("----------------------------------------------");
-         String passwd =c.nextLine();
-         System.out.println("----------------------------------------------");
 
+    static void password() {
+        String passwd = "A123_"; // define the password
+        char[] passwdTab = passwd.toCharArray(); // convert the String to array
+        Scanner c = new Scanner(System.in);
+        System.out.println("------------- Guess the password -------------");
+        System.out.println("It must contain a maximum of 4 characters");
+        System.out.println("----------------------------------------------");
+        String guess = c.nextLine();//input of the password guessed by user
+        char[] guessTab = guess.toCharArray();
+        System.out.println("----------------------------------------------");
+        boolean verify = false;
+        int count = 0;
+            for (int z : passwdTab) {
+                for (int y : guessTab) {
+                    if (z == y) {
+                        count++;
+                    }
+                }
+            }
+            if (count == passwdTab.length) {
+                System.out.println("password correct");
+            } else {
+                System.out.println("password false!");
+                System.out.println("try again ");
+                guess = c.nextLine(); // I want to restart the operation !!!!!
+            }
 
-
-
-
-
-
-
-
-
-     }
-
-
+    }
 }
+
+
+
+
