7e107e79e6bc959ce7c5e7fcdce325cb1c7cf0e2
awatef-toutou <awatefnf@hotmail.fr>
2025-11-14 16:51:51 +0100

    evening

--- src/classJava/job15/JeuDeDe.java
+++ src/classJava/job15/JeuDeDe.java
@@ -23,5 +23,6 @@ public class JeuDeDe {
      public void Jeux ()
      {
 
+
      }
 }
--- src/classJava/job15/Main.java
+++ src/classJava/job15/Main.java
@@ -5,7 +5,7 @@ import classJava.job14.CompteClient;
 public class Main {
     public static void main (String[] args)
     {
-        Joueur J1 = new Joueur("maxime");
+        Joueur J1 = new Joueur("lea");
         JeuDeDe j1 = new JeuDeDe(J1);
         Joueur J2 = new Joueur("awatef");
         JeuDeDe j2 = new JeuDeDe(J2);
